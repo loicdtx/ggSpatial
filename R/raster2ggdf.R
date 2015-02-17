5dd38fcac7c5d76ef7e7b205ce1a71e869551d30
Loïc Dutrieux <loic.dutrieux@gmail.com>
2015-02-17 18:23:48 +0100

    raster2ggdf function finished and documented

---
+++ R/raster2ggdf.R
@@ -0,0 +1,62 @@
+#' Prepare a raster object to plot using geom_raster or geom_tile
+#' 
+#' @param x raster object
+#' 
+#' @author Loic Dutrieux
+#' 
+#' @import raster
+#' @import magrittr
+#' 
+#' @export
+#' 
+#' @examples 
+#' 
+#' library(raster)
+#' library(ggplot2)
+#'
+#' r <- raster(nrow = 10, ncol = 20)
+#' r[] <- rnorm(ncell(r))
+#' 
+#' ggr <- raster2ggdf(r)
+#' 
+#' ggplot() +
+#'     geom_raster(data = ggr, aes(x,y,fill = values)) +
+#'     coord_equal() +
+#'     scale_x_continuous(name = 'Lat', expand=c(0, 0)) +
+#'     scale_y_continuous(name = 'Long', expand=c(0, 0))
+#' 
+#' # Also works for rasterStackBricks
+#' 
+#' b <- brick(nrow = 10, ncol = 20, nl = 3)
+#' b[] <- rnorm(ncell(b) * nlayers(b))
+#' names(b) <- c('a', 'b', 'c')
+#' ggb <- raster2ggdf(b)
+#' 
+#' ggplot() +
+#'     geom_raster(data = ggb, aes(x,y,fill = values.a)) +
+#'     coord_equal() +
+#'     scale_x_continuous(name = 'Lat', expand=c(0, 0)) +
+#'     scale_y_continuous(name = 'Long', expand=c(0, 0))
+#' 
+#' # For facet grid/wrap, you can use tidyr to reshape the dataframe produced by the function
+#' library(tidyr)
+#' 
+#' ggb2 <- gather(ggb, layer, values, -x, -y)
+#' 
+#' ggplot() +
+#'     geom_raster(data = ggb2, aes(x,y,fill = values)) +
+#'     coord_equal() +
+#'     scale_x_continuous(name = 'Lat', expand=c(0, 0)) +
+#'     scale_y_continuous(name = 'Long', expand=c(0, 0))+
+#'     facet_wrap(~ layer)
+
+raster2ggdf <- function(x) {
+    
+    xy <- xyFromCell(x, seq_len(ncell(x)))
+    out <- x %>%
+        getValues() %>%
+        data.frame(values = .) %>%
+        cbind(xy)
+}
+
+
